f55a0e6bb09468f4e127262860965e409de523c7
Nick Eubank <nick@nickeubank.com>
2022-09-15 16:35:47 -0400

    convert to pytest

--- actual_autograder/setup.sh
+++ actual_autograder/setup.sh
@@ -9,10 +9,18 @@ echo "export PATH=/root/miniconda3/bin:\$PATH" >> /root/.bashrc
 export PATH=/root/miniconda3/bin:$PATH
 
 # install dependencies with conda
-# conda install mamba -n base -c conda-forge
-# mamba env create -f /autograder/source/environment.yml
+conda install mamba -n base -c conda-forge
+mamba env create -f /autograder/source/environment.yml
 
-conda env create -f /autograder/source/environment.yml
+# conda env create -f /autograder/source/environment.yml
 
 # set conda shell
 conda init --all
+
+# Add little helper
+source /root/miniconda3/etc/profile.d/conda.sh
+conda activate grader
+cd /autograder/source/
+git clone https://github.com/ucsb-gradescope-tools/pytest_utils.git
+cd pytest_utils
+pip install -e .
